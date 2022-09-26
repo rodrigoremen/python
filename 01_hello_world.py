a74ba7cb07ed7abf747cbfe6be0ec80ed3826583
Rodrigo Dorantes <106606020+rodrigoremen@users.noreply.github.com>
2022-09-26 17:52:50 -0500

    Add files via upload

---
+++ 01_hello_world.py
@@ -0,0 +1,15 @@
+print("olaaaaa ya se python")
+print (type(90))
+print (type(90.2))
+print (type("ola"))
+print (type(True))
+
+x=3
+print(x*5)
+15
+print("Cisco"*x)
+'CiscoCiscoCisco'
+
+x=3
+print("este valor es x:",x)
+
